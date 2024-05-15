1904798becd69f434e5d6c3ae02b47ed8f5019b3
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-05-15 11:43:09 +0200

    boilerplate met: ejs, express werkent

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// set the view engine to ejs
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// Port
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
