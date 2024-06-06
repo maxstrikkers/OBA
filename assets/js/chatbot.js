f38bd4512c16a165fa605528a16d46135a4ff92c
maxstrikkers <maxstrikkers@outlook.com>
2024-06-06 11:24:21 +0200

    Update chatbot.js

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -2,16 +2,6 @@
 let chatCleared = false;
 let timeouts = []; // Array om timeouts bij te houden
 
-
-//chatbot component variables
-const chatbot = {
-    main: document.getElementById("chatbot-main"),
-    searchbar: document.getElementById("search-bar"),
-    suggestedForm: document.getElementById("suggested-form"),
-    searchForm: document.querySelector(".search-form"),
-    newchatButton: document.getElementById("new-chat-button")
-};
-
 // Event listener om te zorgen dat de functies worden aangeroepen wanneer de DOM is geladen
 document.addEventListener("DOMContentLoaded", function () {
   showWelcomeMessage();
@@ -20,17 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
 // Functie om een typende indicatie te tonen
 
 function showTypingBubble(isWelcomeMessage) {
-    const typingBubble = document.createElement("div");
-    typingBubble.className = "bubble typing";
+  const typingBubble = document.createElement("div");
+  typingBubble.className = "bubble typing";
 
-    // Disable elements while typing bubble is shown
-    chatbot.searchbar.disabled = true;
-    chatbot.searchForm.classList.add("disabled");
-    Array.from(
-        document.getElementsByClassName("chat-button-grid")[0].children
-    ).forEach((button) => {
-        button.disabled = true;
-    });
+  // Disable elements while typing bubble is shown
+  document.getElementById("search-bar").disabled = true;
+  document.querySelector(".search-form").classList.add("disabled");
+  Array.from(
+    document.getElementsByClassName("chat-button-grid")[0].children
+  ).forEach((button) => {
+    button.disabled = true;
+  });
 
   // Add SVG to the typingBubble
   typingBubble.innerHTML = `
@@ -47,25 +37,25 @@ function showTypingBubble(isWelcomeMessage) {
     </svg>
     `;
 
-
-    chatbot.main.appendChild(typingBubble);
+  const chatbotMain = document.getElementById("chatbot-main");
+  chatbotMain.appendChild(typingBubble);
 
   if (isWelcomeMessage == "true") {
     // Remove typing indication after a certain time and enable the buttons along with search bar
     const timeout = setTimeout(() => {
       typingBubble.remove();
 
-            // Enable elements back after removing typing bubble
-            chatbot.searchbar.disabled = false;
-            chatbot.searchForm.classList.remove("disabled");
-            Array.from(
-                document.getElementsByClassName("chat-button-grid")[0].children
-            ).forEach((button) => {
-                button.disabled = false;
-            });
-        }, 1500);
-        timeouts.push(timeout); // Add timeout to the array
-    }
+      // Enable elements back after removing typing bubble
+      document.getElementById("search-bar").disabled = false;
+      document.querySelector(".search-form").classList.remove("disabled");
+      Array.from(
+        document.getElementsByClassName("chat-button-grid")[0].children
+      ).forEach((button) => {
+        button.disabled = false;
+      });
+    }, 1500);
+    timeouts.push(timeout); // Add timeout to the array
+  }
 
   if (chatCleared) {
     chatCleared = false;
@@ -77,166 +67,142 @@ function showTypingBubble(isWelcomeMessage) {
 
 // Functie om bubbles te genereren
 function createBubble(content, className) {
-    const bubble = document.createElement("div");
-    bubble.className = `bubble ${className}`;
-    bubble.textContent = content;
-    return bubble;
+  const bubble = document.createElement("div");
+  bubble.className = `bubble ${className}`;
+  bubble.textContent = content;
+  return bubble;
 }
 
 // Functie om welkomsberichten weer te geven
 function showWelcomeMessage() {
-    let delay = 0;
-    const welcomeMessages = [
-        "Welkom bij de zoekassistent van de OBA. Begin door uw zoekopdracht onderaan het scherm te typen.",
-        // 'Tip: Je kunt ook boven in een knop aanklikken!'
-    ];
+  let delay = 0;
+  const welcomeMessages = [
+    "Welkom bij de zoekassistent van de OBA. Begin door uw zoekopdracht onderaan het scherm te typen.",
+    // 'Tip: Je kunt ook boven in een knop aanklikken!'
+  ];
 
-    welcomeMessages.forEach((message) => {
-        const timeout = setTimeout(() => {
-            if (chatCleared) {
-                chatCleared = false;
-                return;
-            }
-            const typingBubble = showTypingBubble("true");
-            const innerTimeout = setTimeout(() => {
-                if (chatCleared) {
-                    chatCleared = false;
-                    return;
-                }
-                const bubble = createBubble(message, "left");
-                chatbot.main.appendChild(bubble);
-            }, 1500);
-            timeouts.push(innerTimeout); // Voeg de inner timeout toe aan de array
-        }, delay);
-        timeouts.push(timeout); // Voeg de timeout toe aan de array
-        delay += 2000;
-    });
+  welcomeMessages.forEach((message) => {
+    const timeout = setTimeout(() => {
+      if (chatCleared) {
+        chatCleared = false;
+        return;
+      }
+      const typingBubble = showTypingBubble("true");
+      const innerTimeout = setTimeout(() => {
+        if (chatCleared) {
+          chatCleared = false;
+          return;
+        }
+        const bubble = createBubble(message, "left");
+        document.getElementById("chatbot-main").appendChild(bubble);
+      }, 1500);
+      timeouts.push(innerTimeout); // Voeg de inner timeout toe aan de array
+    }, delay);
+    timeouts.push(timeout); // Voeg de timeout toe aan de array
+    delay += 2000;
+  });
 }
 
 // Event listener voor het versturen van de formulieren
 document
-    .querySelectorAll("form.suggested-form, form.search-form")
-    .forEach((form) => {
-        form.addEventListener("submit", function (event) {
-            event.preventDefault();
-            const data = {};
+  .querySelectorAll("form.suggested-form, form.search-form")
+  .forEach((form) => {
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const data = {};
 
-            createTempBubbles(form, data);
-            showTypingBubble();
-
-            placeholderResults()
-
-            // Verzamelen van bestaande chatbubbels
-            const bubbles = document.querySelectorAll(".bubble");
-            let bubbleData = [];
-            bubbles.forEach((bubble) => {
-                const isRight = bubble.classList.contains("right");
-                const isLeft = bubble.classList.contains("left");
-                if (isRight || isLeft) {
-                    bubbleData.push({
-                        content: bubble.innerText,
-                        class: isRight ? "right" : "left",
-                    });
-                }
-            });
-
-            data.bubbles = bubbleData;
-            const url = this.action;
-
-            submitFormData(url, data);
-            document.getElementById("suggested-form").classList.add("hidden");
-        });
-    });
-
-function createTempBubbles(form, data) {
-    if (form.classList.contains("suggested-form")) {
-        const formData = form.querySelector("[type=submit]").value;
+      if (form.classList.contains("suggested-form")) {
+        const formData = event.submitter.value;
         data["query"] = formData;
-        const bubble = document.createElement("div");
-        bubble.className = `right temporaryBubble`;
-        bubble.innerHTML = `<p>${formData}</p>`;
-        chatbot.main.appendChild(bubble);
-    } else if (form.classList.contains("search-form")) {
-        const formData = new FormData(form);
+      } else if (form.classList.contains("search-form")) {
+        const formData = new FormData(this);
         formData.forEach((value, key) => {
-            data[key] = value;
+          data[key] = value;
         });
+
+        const chatbotMain = document.getElementById("chatbot-main");
         const bubble = document.createElement("div");
         bubble.className = `right temporaryBubble`;
-        bubble.innerHTML = `<p>${chatbot.searchbar.value}</p>`;
-        chatbot.main.appendChild(bubble);
-        chatbot.searchbar.value = "";
-    }
-}
+        bubble.innerHTML = `<p>${
+          document.getElementById("search-bar").value
+        }</p>`;
+        chatbotMain.appendChild(bubble);
+        document.getElementById("search-bar").value = "";
+      }
 
-function placeholderResults() {
-    // Weghalen van geen resultaten text
-    document.querySelector(".empty-state").classList.add("hidden");
+      const typingBubble = showTypingBubble();
+      document.querySelector(".empty-state").classList.add("hidden");
 
-    document.getElementById("results-section").innerHTML = ``;
-    const chatbotMain = document.getElementById("chatbot-main");
-    const bubble = document.createElement("div");
-    bubble.className = `right temporaryBubble`;
-    bubble.innerHTML = `<p>${document.getElementById("search-bar").value}</p>`;
-    chatbotMain.appendChild(bubble);
-    document.getElementById("search-bar").value = "";
-
-    showTypingBubble();
-    document.querySelector(".empty-state").classList.add("hidden");
-
-    document.getElementById("results-section").innerHTML = `
+      document.getElementById("results-section").innerHTML = `
         <article class="placeholder-loading-img"></article>
         <article class="placeholder-loading-img"></article>
         <article class="placeholder-loading-img"></article>
         <article class="placeholder-loading-img"></article>
         <article class="placeholder-loading-img"></article>`;
-  }
 
+      // Verzamelen van bestaande chatbubbels
+      const bubbles = document.querySelectorAll(".bubble");
+      let bubbleData = [];
+      bubbles.forEach((bubble) => {
+        const isRight = bubble.classList.contains("right");
+        const isLeft = bubble.classList.contains("left");
+        if (isRight || isLeft) {
+          bubbleData.push({
+            content: bubble.innerText,
+            class: isRight ? "right" : "left",
+          });
+        }
+      });
 
-  function submitFormData(url, data) {
-    // Versturen van de gegevens via een fetch-aanroep
-    fetch(url, {
+      data.bubbles = bubbleData;
+      const url = this.action;
+
+      // Versturen van de gegevens via een fetch-aanroep
+      fetch(url, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        // Verwijder bestaande chatbubbels
-        const existingChats = document.querySelectorAll(".bubble");
-        existingChats.forEach((chat) => chat.remove());
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Verwijder bestaande chatbubbels
+          const existingChats = document.querySelectorAll(".bubble");
+          existingChats.forEach((chat) => chat.remove());
 
-        // Voeg nieuwe berichten toe
-        const messageData = data.messages;
-        messageData.forEach((message) => {
+          // Voeg nieuwe berichten toe
+          const messageData = data.messages;
+          messageData.forEach((message) => {
             const chatbotMain = document.getElementById("chatbot-main");
             const bubble = document.createElement("div");
             bubble.className = `bubble ${message.class}`;
             bubble.innerHTML = `<p>${message.content}</p>`;
             chatbotMain.appendChild(bubble);
-        });
+          });
 
-        // Verwijder tijdelijke elementen
-        const temporaryElements = document.querySelectorAll(".temporaryBubble");
-        temporaryElements.forEach((element) => element.remove());
+          // Verwijder tijdelijke elementen
+          const temporaryElements =
+            document.querySelectorAll(".temporaryBubble");
+          temporaryElements.forEach((element) => element.remove());
 
-        // Enable elements back after removing typing bubble
-        document.getElementById("search-bar").disabled = false;
-        document.querySelector(".search-form").classList.remove("disabled");
-        Array.from(document.getElementsByClassName("chat-button-grid")[0].children).forEach((button) => {
+          // Enable elements back after removing typing bubble
+          document.getElementById("search-bar").disabled = false;
+          document.querySelector(".search-form").classList.remove("disabled");
+          Array.from(
+            document.getElementsByClassName("chat-button-grid")[0].children
+          ).forEach((button) => {
             button.disabled = false;
-        });
+          });
 
-        // Scroll naar onderaan de chatbot
-        scrollToBottom("chatbot-main");
+          // Scroll naar onderaan de chatbot
+          scrollToBottom("chatbot-main");
 
-        // Voeg resultaten toe aan de results section
-        const resultData = data.results;
-        const resultsSection = document.getElementById("results-section");
-        resultsSection.innerHTML = ""; // Maak de results section leeg voordat je nieuwe resultaten toevoegt
-        resultData.forEach((result) => {
+          // Voeg resultaten toe aan de results section
+          const resultData = data.results;
+          const resultsSection = document.getElementById("results-section");
+          resultsSection.innerHTML = ""; // Maak de results section leeg voordat je nieuwe resultaten toevoegt
+          resultData.forEach((result) => {
             console.log(result.document);
 
             // Maak een nieuw article element
@@ -255,13 +221,12 @@ function placeholderResults() {
 
             // Voeg het article toe aan de results section
             resultsSection.appendChild(article);
-        });
-    })
-    .catch((error) => console.error("Error:", error));
-
-    document.getElementById("suggested-form").classList.add("hidden");
-}
-
+          });
+        })
+        .catch((error) => console.error("Error:", error));
+      document.getElementById("suggested-form").classList.add("hidden");
+    });
+  });
 
 // Functie om naar de onderkant van het chatvenster te scrollen
 function scrollToBottom(elementId) {
@@ -270,27 +235,28 @@ function scrollToBottom(elementId) {
 
   const scrollTo = element.scrollHeight - element.clientHeight;
 
-    // Smooth scroll
-    element.scrollTo({
-        top: scrollTo,
-        behavior: "smooth",
-    });
+  // Smooth scroll
+  element.scrollTo({
+    top: scrollTo,
+    behavior: "smooth",
+  });
 }
 
 // Event listener voor de "new chat"-knop om de chat te wissen en opnieuw welkomsberichten te tonen
-chatbot.newchatButton.addEventListener("click", function () {
-    chatbot.main.innerHTML = "";
-    document.getElementById("results-section").innerHTML = "";
-    document.querySelector(".empty-state").classList.remove("hidden");
+const newChatButton = document.getElementById("new-chat-button");
+newChatButton.addEventListener("click", function () {
+  document.getElementById("chatbot-main").innerHTML = "";
+  document.getElementById("results-section").innerHTML = "";
+  document.querySelector(".empty-state").classList.remove("hidden");
 
-    chatCleared = true;
+  chatCleared = true;
 
-    // Annuleer alle timeouts
-    timeouts.forEach((timeout) => clearTimeout(timeout));
-    timeouts = []; // Leeg de array
+  // Annuleer alle timeouts
+  timeouts.forEach((timeout) => clearTimeout(timeout));
+  timeouts = []; // Leeg de array
 
-    // Reset chatCleared en start welkomsberichten opnieuw
-    chatCleared = false;
-    chatbot.suggestedForm.classList.remove("hidden");
-    showWelcomeMessage();
+  // Reset chatCleared en start welkomsberichten opnieuw
+  chatCleared = false;
+  document.getElementById("suggested-form").classList.remove("hidden");
+  showWelcomeMessage();
 });
